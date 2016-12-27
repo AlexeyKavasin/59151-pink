97db7801d4bd0123da0f25d11d587b937a2530a7
akavasin <azamatdj@gmail.com>
2016-12-27 23:03:04 +0400

    Доработки (пиксельперфект, кавычки везде все же заменены на двойные)

--- js/main-menu.js
+++ js/main-menu.js
@@ -1,17 +1,17 @@
-var menuItems = document.querySelector('.main-menu__items');
-var toggleBtn = document.querySelector('.hamburger-menu__btn');
-var crossIcon = document.querySelector('.hamburger-menu__cross-icon');
-var hamIcon = document.querySelector('.hamburger-menu__ham-icon');
+var menuItems = document.querySelector(".main-menu__items");
+var toggleBtn = document.querySelector(".hamburger-menu__btn");
+var crossIcon = document.querySelector(".hamburger-menu__cross-icon");
+var hamIcon = document.querySelector(".hamburger-menu__ham-icon");
 
-menuItems.classList.remove('no-js');
-hamIcon.classList.remove('no-js');
-hamIcon.classList.remove('hidden');
+menuItems.classList.remove("no-js");
+hamIcon.classList.remove("no-js");
+hamIcon.classList.remove("hidden");
 
-crossIcon.classList.add('hidden');
-menuItems.classList.add('hidden');
+crossIcon.classList.add("hidden");
+menuItems.classList.add("hidden");
 
-toggleBtn.addEventListener('click', function() {
-  menuItems.classList.toggle('hidden');
-  crossIcon.classList.toggle('hidden');
-  hamIcon.classList.toggle('hidden');
+toggleBtn.addEventListener("click", function() {
+  menuItems.classList.toggle("hidden");
+  crossIcon.classList.toggle("hidden");
+  hamIcon.classList.toggle("hidden");
 });
--- js/map.js
+++ js/map.js
@@ -2,21 +2,21 @@ var myMap;
 ymaps.ready(init);
 
 function init() {
-  myMap = new ymaps.Map('contacts-map', {
+  myMap = new ymaps.Map("contacts-map", {
     center: [59.936249884406166, 30.321582336587152],
-    controls: ['zoomControl'],
+    controls: ["zoomControl"],
     zoom: 15
   });
   myMap.geoObjects.add(new ymaps.Placemark(myMap.getCenter(), {
-    balloonContentHeader: '<strong>Pink!</strong>',
-    balloonContentBody: 'Адрес: ул. Большая Конюшенная, 19/8',
-    balloonContentFooter: 'Телефон: +7 (812) 555-66-66'
+    balloonContentHeader: "<strong>Pink!</strong>",
+    balloonContentBody: "Адрес: ул. Большая Конюшенная, 19/8",
+    balloonContentFooter: "Телефон: +7 (812) 555-66-66"
   }, {
-    iconLayout: 'default#image',
+    iconLayout: "default#image",
     iconImageSize: [36, 35],
     iconImageOffset: [-18, -18],
-    iconImageHref: 'img/icon-map-marker.svg'
+    iconImageHref: "img/icon-map-marker.svg"
   }));
 
-  myMap.behaviors.disable('scrollZoom');
+  myMap.behaviors.disable("scrollZoom");
 }
